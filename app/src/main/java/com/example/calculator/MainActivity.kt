7dfee5c69ac430d2fbc0c1081d1f99db12e93ab6
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-12 17:13:05 +0300

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,87 +5,75 @@ import android.view.inputmethod.InputBinding
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
-    lateinit var etNum1: EditText
-    lateinit var etNum2: EditText
-    lateinit var btnAdd: Button
-    lateinit var btnSubtract: Button
-    lateinit var btnMultiply: Button
-    lateinit var btnDivide: Button
-    lateinit var tvResult: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        castViews()
 
-        btnAdd.setOnClickListener{
+        binding.btnAdd.setOnClickListener{
             validate("+")
         }
 
-        btnSubtract.setOnClickListener{
+        binding.btnSubstract.setOnClickListener{
             validate("-")
         }
 
-        btnMultiply.setOnClickListener{
+        binding.btnMultiply.setOnClickListener{
             validate("*")
         }
 
-        btnDivide.setOnClickListener{
+        binding.btnDivide.setOnClickListener{
             validate("/")
 
        }
     }
-        fun castViews(){
-            etNum1 = binding.etNum1
-            etNum2 = binding.etNum2
-            btnAdd = binding.btnAdd
-            btnSubtract = binding.btnSubstract
-            btnMultiply = binding.btnMultiply
-            btnDivide = binding.btnDivide
-            tvResult = binding.tvResult
-        }
 
         fun validate(operation: String){
-            val num1 = etNum1.text.toString()
-            val num2 = etNum2.text.toString()
+            val num1 = binding.etNum1.text.toString()
+            val num2 = binding.etNum2.text.toString()
             var errorT = false
             if (num1.isBlank()){
                 errorT = true
-                etNum1.error = "Num 1 is required" //setting error bcz etNum1 inherites the attributes
+                binding.etNum1.error = "Num 1 is required" //setting error bcz etNum1 inherites the attributes
                                                  // form Class TextView, one of it properties is error
             }
             if (num2.isBlank()){
                 errorT = true
-                etNum2.error = "Num 2 is required"
+                binding.etNum2.error = "Num 2 is required"
             }
             if (!errorT){
                 calculate(num1.toDouble(), num2.toDouble(), operation)
             }
         }
-        fun calculate(num1: Double, num2:Double, operation: String){
-            var result = 0.0
-            when(operation){
-                "+" -> {
-                    result = num1 + num2
+    fun calculate(num1: Double, num2: Double, sign: String) {
+        var result = 0.0
+        when (sign) {
+            "+" -> result = (num1 + num2)
+            "-" -> result = (num1 - num2)
+            "*" -> result = (num1 * num2)
+            "/" -> {
+                if (num2 == 0.0) {
+                    binding.tvResult.text = "Cannot divide by zero"
+                    return
                 }
-                "-" -> {
-                    result = num1 - num2
-                }
-                "*" -> {
-                    result = num1 * num2
-                }
-                "/" -> {
-                    result = num1 / num2
-                }
+                result = num1 / num2
             }
-            tvResult.text = result.toString()
-            //Anything editable in Android is perceive to be text, so to convert it to any other data type we first convert it to string.
+
+        }
+        binding.tvResult.text = result.toString()
+        if (sign == "/") {
+            binding.tvResult.text = result.toString()
+        } else {
+            binding.tvResult.text = if (num1 % 1 == 0.0 && num2 % 1 == 0.0) result.toInt()
+                .toString() else result.toString()
         }
     }
+}
